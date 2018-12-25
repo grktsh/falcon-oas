d45c12b097905c561c5fd83a2a7eda421ea12024
grktsh <grktsh@gmail.com>
2018-12-25 16:22:47 +0900

    Put falcon_oas package into src directory
    https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
 setup(
+    # https://github.com/pypa/setuptools/issues/1136
+    package_dir={'': 'src'},
     use_scm_version={
-        'write_to': 'falcon_oas/__version__.py',
+        'write_to': 'src/falcon_oas/__version__.py',
         'write_to_template': '__version__ = {version!r}\n',
-    }
+    },
 )
