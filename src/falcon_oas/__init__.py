b45239b87b6375d67c7515395c426201321469d9
grktsh <grktsh@gmail.com>
2019-03-06 01:37:27 +0900

    Export __version__

--- src/falcon_oas/__init__.py
+++ src/falcon_oas/__init__.py
@@ -3,5 +3,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from .__version__ import __version__  # noqa: F401
 from .factories import create_api  # noqa: F401
 from .request import Request  # noqa: F401
