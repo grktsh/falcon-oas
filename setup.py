283dd8a5d30845d926c2ddc667be55723fa0f26a
Atsushi Hanaoka <grktsh@gmail.com>
2019-04-19 01:09:22 +0900

    Move package_dir definition to setup.cfg (#7)

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup
 
 setup(
-    # https://github.com/pypa/setuptools/issues/1136
-    package_dir={'': 'src'},
     use_scm_version={
         'write_to': 'src/falcon_oas/__version__.py',
         'write_to_template': '__version__ = {version!r}\n',
-    },
+    }
 )
