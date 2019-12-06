d09c10de34f057490c782d3dcb8dd60414649195
grktsh <grktsh@gmail.com>
2019-12-07 02:05:46 +0900

    Refine Problem class

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -5,69 +5,46 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import falcon
-import jsonschema
+import pytest
 from falcon import testing
 from oas.exceptions import UnmarshalError
 
-from falcon_oas.problems import _Problem
+from falcon_oas.problems import http_error_handler
+from falcon_oas.problems import Problem
 from falcon_oas.problems import serialize_problem
+from falcon_oas.problems import unmarshal_error_handler
 from falcon_oas.problems import UNMARSHAL_PROBLEM_TYPE_URI
-from falcon_oas.problems import UnmarshalProblem
 
 
 def test_problem():
     title = 'title'
     description = 'description'
     http_error = falcon.HTTPBadRequest(title=title, description=description)
-    problem = _Problem(http_error)
+    problem = Problem.from_http_error(http_error)
 
     assert isinstance(problem, falcon.HTTPError)
     assert problem.status == falcon.HTTP_BAD_REQUEST
     assert problem.title == title
     assert problem.description == description
     assert problem.to_dict() == {
-        'title': 'Bad Request',
+        'title': title,
         'status': 400,
         'detail': description,
     }
 
 
-def test_problem_to_dict_without_description():
+def test_problem_to_dict_without_title_and_description():
     http_error = falcon.HTTPBadRequest()
-    problem = _Problem(http_error)
+    problem = Problem.from_http_error(http_error)
 
     assert problem.to_dict() == {'title': 'Bad Request', 'status': 400}
-
-
-def test_unmarshal_problem():
-    unmarshal_error = UnmarshalError(
-        request_body_errors=[
-            jsonschema.ValidationError(
-                'Request body is required', validator='required'
-            )
-        ]
-    )
-    problem = UnmarshalProblem(unmarshal_error)
-
-    assert problem.to_dict() == {
-        'type': UNMARSHAL_PROBLEM_TYPE_URI,
-        'title': 'Unmarshal Error',
-        'status': 400,
-        'request_body': [
-            {
-                'path': [],
-                'validator': 'required',
-                'message': 'Request body is required',
-            }
-        ],
-    }
 
 
 def test_serialize_problem():
     environ = testing.create_environ()
     req = falcon.Request(environ)
     resp = falcon.Response()
-    problem = _Problem(falcon.HTTPBadRequest())
+    problem = Problem.from_http_error(falcon.HTTPBadRequest())
 
     serialize_problem(req, resp, problem)
 
@@ -80,7 +57,7 @@ def test_serialize_problem_accept_json():
     environ = testing.create_environ(headers={'Accept': 'application/json'})
     req = falcon.Request(environ)
     resp = falcon.Response()
-    problem = _Problem(falcon.HTTPBadRequest())
+    problem = Problem.from_http_error(falcon.HTTPBadRequest())
 
     serialize_problem(req, resp, problem)
 
@@ -91,8 +68,33 @@ def test_serialize_problem_accept_html():
     environ = testing.create_environ(headers={'Accept': 'text/html'})
     req = falcon.Request(environ)
     resp = falcon.Response()
-    problem = _Problem(falcon.HTTPBadRequest())
+    problem = Problem.from_http_error(falcon.HTTPBadRequest())
 
     serialize_problem(req, resp, problem)
 
     assert resp.content_type == 'application/json'
+
+
+def test_http_error_handler():
+    http_error = falcon.HTTPBadRequest()
+    req = falcon.Request(testing.create_environ())
+    resp = falcon.Response()
+    params = {}
+
+    with pytest.raises(Problem):
+        http_error_handler(http_error, req, resp, params)
+
+
+def test_unmarshal_error_handler():
+    unmarshal_error = UnmarshalError()
+    req = falcon.Request(testing.create_environ())
+    resp = falcon.Response()
+    params = {}
+
+    with pytest.raises(Problem) as excinfo:
+        unmarshal_error_handler(unmarshal_error, req, resp, params)
+
+    assert excinfo.value.type_uri == UNMARSHAL_PROBLEM_TYPE_URI
+    assert excinfo.value.status == falcon.HTTP_BAD_REQUEST
+    assert excinfo.value.title == 'Unmarshal Error'
+    assert excinfo.value.additional_members == unmarshal_error.to_dict()
