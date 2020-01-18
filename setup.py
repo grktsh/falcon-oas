f2e81e39bc43d5d29e1df1a7b75282c8d793a023
Atsushi Hanaoka <grktsh@gmail.com>
2020-01-19 02:26:11 +0900

    Update setuptools_scm to 3.4 (#20)

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        'write_to': 'src/falcon_oas/__version__.py',
-        'write_to_template': '__version__ = {version!r}\n',
-    }
-)
+setup()
